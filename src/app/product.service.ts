3a14ee804ca14ed7283536065b6156bb242904b1
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2020-10-08 23:17:25 +0200

    Feat: Using Interfaces to Describe Data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { HttpClient } from '@angular/common/http';
-// import { map } from 'rxjs/operators';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+
+import { Album } from './album'
 
 @Injectable()
 export class ProductService {
-  constructor(private _http: Http) {}
+  constructor(private _http: Http) { }
 
   private _albumUrl = '../assets/album.json';
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map(response => response.json());
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map(response => <Album>(response.json()));
   }
 }
